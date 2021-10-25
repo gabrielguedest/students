ac17f95fdd5168e523d53d27f639031e4288e3ef
Gabriel Guedes <gabrielgue10@gmail.com>
2021-10-25 17:07:32 -0300

    Client - Add env variables

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import { App } from './components/App';
 import { ApolloProvider } from '@apollo/client';
 import { apolloClient } from './utils/apolloClient';
--- client/src/utils/apolloClient.js
+++ client/src/utils/apolloClient.js
@@ -3,7 +3,7 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client"
 export const apolloClient = () => {
   return new ApolloClient({
     link: new HttpLink({
-      uri: process.env.GRAPHQL_URL
+      uri: process.env.REACT_APP_GRAPHQL_URL
     }),
     cache: new InMemoryCache(),
     connectToDevTools: true,
